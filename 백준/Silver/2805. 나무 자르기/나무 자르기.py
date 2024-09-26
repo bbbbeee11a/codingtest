5d7dcc1104d03eadff194533f65daa95aac479d7
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2024-09-26 19:46:57 +0900

    [Silver II] Title: 나무 자르기, Time: 1852 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -5,7 +5,6 @@ N, M = map(int, input().split())
 trees = list(map(int, input().strip().split()))
 
 
-# max_height 기준으로 얻을 수 있는 나무 길이 계산
 def get_tree(max_height, trees):
     total = 0
     for tree in trees:
@@ -14,7 +13,6 @@ def get_tree(max_height, trees):
     return total
 
 
-# 이진 탐색으로 최대 절단 높이 찾기
 def get_height(M, trees):
     l = 0
     h = max(trees)
@@ -22,14 +20,14 @@ def get_height(M, trees):
     
     while l <= h:
         mid = (l + h) // 2
-        wood = get_tree(mid, trees)  # mid 기준으로 나무 길이 계산
+        woods = get_tree(mid, trees)
         
-        if wood >= M:  # 필요한 나무 길이보다 크거나 같으면 더 높은 절단 가능
-            result = mid  # 최적 해 후보 갱신
+        if woods >= M:
+            result = mid
             l = mid + 1
-        else:  # 필요한 나무 길이보다 작으면 더 낮게 절단해야 함
+        else:
             h = mid - 1
-    
+
     return result
 
 print(get_height(M, trees))
