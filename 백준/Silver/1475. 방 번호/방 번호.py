5036fd4c8b15aa0c1fca606cde1fe758f5ce5f7a
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2024-11-14 15:37:22 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -14,15 +14,4 @@ if arr[6] + arr[9] >= 2:
     arr[6] = math.ceil((arr[6] + arr[9]) / 2.0)
     arr[9] = 0
 
-while any(num > 0 for num in arr):
-    if all(num <= 1 for num in arr):
-        result += 1
-        break
-        
-    for i in range(9):
-        if arr[i] > 0:
-            arr[i] -= 1
-    result += 1
-
-
-print(result)
+print(max(arr))
