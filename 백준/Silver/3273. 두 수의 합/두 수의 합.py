13acb5f6b6020a4f1e9d24c2870cfa56aede4195
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2024-11-23 22:38:12 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,27 +1,22 @@
 import sys
-
 input = sys.stdin.readline
 
 n = int(input())
-nums = list(map(int, input().split()))
-x = int(input())
+arr = sorted(list(map(int, input().split())))
+target = int(input())
 
-left = 0
-right = n - 1
+left, right = 0, n - 1
 cnt = 0
 
-nums.sort()
-
 while left < right:
-    current_sum = nums[left] + nums[right]
-
-    if current_sum == x:
-        left += 1
-        right -= 1
+    s = arr[left] + arr[right]
+    if s == target:
         cnt += 1
-    elif current_sum < x:
         left += 1
-    else:
         right -= 1
+    elif s > target:
+        right -= 1
+    else:  # s < target
+        left += 1
 
 print(cnt)
