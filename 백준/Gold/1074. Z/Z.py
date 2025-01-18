344ce4f8b49706699eebfad2eef651664df4b999
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2025-01-18 17:03:07 +0900

    [Gold V] Title: Z, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,17 +1,10 @@
 N, r, c = map(int, input().split())
 
-def sol2(N, r, c):
+def sol(N, r, c):
     if N == 0:
         return 0
 
-    half = 2 ** (N - 1)
-    if r < half and c < half:  # 1번 사각형(제2사분면)
-        return sol2(N - 1, r, c)
-    elif r < half and c >= half:  # 2번 사각형(제1사분면)
-        return half * half + sol2(N - 1, r, c - half)
-    elif r >= half and c < half:  # 3번 사각형(제3사분면)
-        return 2 * half * half + sol2(N - 1, r - half, c)
+    return 2 * (r % 2) + (c % 2) + 4 * sol(N - 1, int(r / 2), int(c / 2))
 
-    return 3 * half * half + sol2(N - 1, r - half, c - half)  # 4번 사격형(제4사분면)
 
-print(sol2(N, r, c))
+print(sol(N, r, c))
