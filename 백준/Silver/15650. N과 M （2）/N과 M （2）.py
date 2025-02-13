de1ed239a8962acf68501890fca8d3209eab4ed6
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2025-02-13 23:44:16 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,7 +1,21 @@
-import itertools
+arr = [0] * 9
+is_used = [0] * 9
+
+
+def func(k):
+    if k == M:
+        for i in range(M):
+            print(arr[i], end=' ')
+        print()
+        return
+
+    for i in range(1, N + 1):
+        if not is_used[i] and i > arr[k - 1]:
+            arr[k] = i
+            is_used[i] = 1
+            func(k + 1)
+            is_used[i] = 0
 
 N, M = map(int, input().split())
-nCr = itertools.combinations(list(i for i in range(1, N + 1)), M)
-for item in nCr:
-    print(*item)
-    
+
+func(0)
