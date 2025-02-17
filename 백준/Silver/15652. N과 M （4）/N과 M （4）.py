fe642676bf89c980794aceafc861cdb6d10e1866
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2025-02-18 01:07:31 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,17 +1,17 @@
-arr = [0] * 8
+arr = [0] * 9
 
 def func(k):
-    if k == M:
-        for i in range(M):
+    if k > M:
+        for i in range(1, M + 1):
             print(arr[i], end=' ')
         print()
         return
 
-    for i in range(1, N + 1):
-        if k == 0 or i >= arr[k - 1]:
-            arr[k] = i
-            func(k + 1)
+    for i in range(arr[k - 1], N + 1):
+        arr[k] = i
+        func(k + 1)
 
 N, M = map(int, input().split())
 
-func(0)
+arr[0] = 1
+func(1)
