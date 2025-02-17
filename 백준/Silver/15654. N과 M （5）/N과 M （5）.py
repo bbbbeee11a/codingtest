4c59810c6cd8df6c64196a396db24eb90635b4d6
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2025-02-18 01:38:00 +0900

    [Silver III] Title: N과 M (5), Time: 176 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+ans = [0] * 8
+
+def func(k):
+    if k == M:
+        print(*ans[:M])
+        return
+
+    for i in range(N):
+        if arr[i]:
+            ans[k] = arr[i]
+            temp = arr[i]
+            arr[i] = 0
+            func(k + 1)
+            arr[i] = temp
+
+func(0)
