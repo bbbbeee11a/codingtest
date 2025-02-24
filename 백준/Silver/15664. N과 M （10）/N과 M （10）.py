00ba686a6bd94665aa57ef6a5882c8fc44c31119
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2025-02-24 22:11:21 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+arr = (sorted(list(map(int, input().split()))))
+visited = [0] * N
+ans = []
+
+
+def sol(k):
+    check = 0
+
+    if k == M:
+        print(*ans)
+        return
+
+    for i in range(N):
+        if check != arr[i] and not visited[i]:
+            if len(ans) == 0 or arr[i] >= ans[-1]:
+                ans.append(arr[i])
+                visited[i] = 1
+                check = arr[i]
+                sol(k + 1)
+                ans.pop()
+                visited[i] = 0
+
+
+sol(0)
